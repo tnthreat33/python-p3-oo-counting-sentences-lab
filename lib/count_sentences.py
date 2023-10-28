67fc26b9bc7285ee2aa9f3540b62059d56678b3b
Rachel Hamby <tnthreat33@gmail.com>
2023-10-28 13:29:42 -0400

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value = ""):
+    self._value = value
+    
+  def get_value(self):
+    return self._value
+
+  def set_value(self, stringVal):
+    if (type(stringVal) == str):
+      self._value = stringVal
+    else:
+      print("The value must be a string.")
+  
+  value = property(get_value, set_value)
